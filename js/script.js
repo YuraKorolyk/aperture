0f71e793635bd72117c4a15ed5fa4064d117864f
Yurii <yura.korolyk@gmail.com>
2022-12-08 23:25:57 +0200

    first adaptiv

---
+++ js/script.js
@@ -0,0 +1,10 @@
+'use strict'
+const burger = document.querySelector('.header__burger');
+const menu = document.querySelector('.header__nav');
+const body = document.querySelector('body');
+
+burger.addEventListener('click', ()=>{
+   burger.classList.toggle('active');
+   menu.classList.toggle('active');
+   body.classList.toggle('lock');
+});
